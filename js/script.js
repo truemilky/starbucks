a2aa5ace4dc2340e3188fe5daaffd014d7dd1f97
truemilky <truemilky@gmail.com>
2025-12-13 17:34:25 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,123 @@
+const burgerMenu = document.querySelector(".header-burger");
+const headerMobileMenu = document.querySelector(".header-mobile-menu");
+const headerMobile = document.querySelector(".header-mobile");
+const headerMobileLinks = document.querySelectorAll(".header-mobile-menu a");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  headerMobileMenu.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+  headerMobile.classList.toggle("active");
+});
+
+headerMobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    headerMobileMenu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+    headerMobile.classList.toggle("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (!target) return;
+
+    const offset = 50;
+
+    const topPosition =
+      target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+    window.scrollTo({
+      top: topPosition,
+      behavior: "smooth",
+    });
+  });
+});
+
+const video = document.getElementById("aboutVideo");
+const playBtn = document.getElementById("playBtn");
+
+video.removeAttribute("controls");
+
+playBtn.addEventListener("click", () => {
+  video.setAttribute("controls", "controls");
+  playBtn.style.display = "none";
+  video.play();
+});
+
+function setPoster() {
+  if (window.innerWidth >= 768) {
+    video.poster = "../images/about-destop.png";
+  } else {
+    video.poster = "../images/about.png";
+  }
+}
+
+setPoster();
+window.addEventListener("resize", setPoster);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".products-slider", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    initialSlide: 1,
+    centeredSlides: true,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        initialSlide: 1,
+      },
+    },
+    
+  });
+});
+
+const scrollContainer = document.querySelector(".events-slider");
+const scrollBtn = document.querySelector(".scroll-left-btn");
+
+scrollContainer.addEventListener("scroll", () => {
+  if (scrollContainer.scrollLeft > 10) {
+    scrollBtn.classList.add("active");
+    scrollBtn.removeAttribute("disabled");
+  } else {
+    scrollBtn.classList.remove("active");
+    scrollBtn.setAttribute("disabled", true);
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  scrollContainer.scrollTo({
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.getElementById("scrollToTop").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    headerMobile.classList.add("scrolled");
+  } else {
+    headerMobile.classList.remove("scrolled");
+  }
+});
